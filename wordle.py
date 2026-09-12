eba6e6f2d7ec31d264dcb6c616c7867824f86f12
Aviraj-Pol <avirajvpol@gmail.com>
2026-09-12 17:25:37 +0530

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -1,69 +1,57 @@
 from random import choice
 
-
 def choose_word():
     words = ["apple", "caped", "soupy", "dated", "sorts"]
-    wordss = "apple"
-    return wordss
+    #return choice(words)
+    return "apple"
 
-
-def checkchoice(hashmap, result):
+def checkchoice(result, original_hashmap):
     val = input_value()
-    if len(val) > 5:
-        checkchoice(hashmap,result)
-    else:
-        input_val = list(val)
-        
-        hashmap1 = {}
-        
-        for i in range(5):
-            if input_val[i] in hashmap1:
-                hashmap1[input_val[i]] += 1
-            else:
-                hashmap1[input_val[i]] = 1
-        
-        arr1 = []
-        
-        for i in range(5):
-            if input_val[i] == result[i]:
-                arr1.append("green")
-                hashmap[input_val[i]] -= 1
-                    
-            elif hashmap.get(input_val[i], 0) > 0:
-                arr1.append("yellow")
-                hashmap[input_val[i]] -= 1
-        
-            else:
-                arr1.append("grey")
-        print("input val:", input_val)
-        print(arr1)
-        
-        if input_val == result:
-            print("correct guess")
-        else:
-            checkchoice(hashmap, result)
-            
     
+    if len(val) != 5:
+        print("Please enter exactly a 5 letter word.")
+        checkchoice(result, original_hashmap)
+        return
+
+    input_val = list(val)
+    
+    hashmap = original_hashmap.copy()
+    
+    arr1 = ["grey"] * 5
+    
+    for i in range(5):
+        if input_val[i] == result[i]:
+            arr1[i] = "green"
+            hashmap[input_val[i]] -= 1
+            
+
+    for i in range(5):
+        if arr1[i] != "green" and hashmap.get(input_val[i], 0) > 0:
+            arr1[i] = "yellow"
+            hashmap[input_val[i]] -= 1
+
+    print("input val:", input_val)
+    print(arr1)
+    
+    if input_val == result:
+        print("correct guess")
+    else:
+        checkchoice(result, original_hashmap)
 
 def input_value():
     val = input("Enter your 5 letter word: ")
+    if val == "exit":
+        exit()
     return val.lower()
 
-
 def main():
-
     result = list(choose_word())
     hashmap = {}
 
-    for i in range(5):
+    for char in result:
+        hashmap[char] = hashmap.get(char, 0) + 1
 
-        if result[i] in hashmap:
-            hashmap[result[i]] += 1
-        else:
-            hashmap[result[i]] = 1
-
-    checkchoice(hashmap, result)
-
+    checkchoice(result, hashmap)
 
 if __name__ == "__main__":
     main()
